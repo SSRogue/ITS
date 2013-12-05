003d4367cd9632a91fde7dce82f5559d822f3641
chen203g <chen203g@142.3.27.184>
2013-12-04 19:00:13 -0600

    new commit

---
+++ src/its/LogOut.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package its;
+
+/**
+ *
+ * @author chen203g
+ */
+public class LogOut {
+    
+    public LogOut ()
+    {
+        
+    }
+    
+}
---
+++ src/its/Login.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package its;
+
+/**
+ *
+ * @author chen203g
+ */
+public class Login {
+    
+}
